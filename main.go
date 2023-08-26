39e86630120294c0ea4b85ec58cf6f3f939242a0
Jimmy <jimmy.lu@smartdesign.com.tw>
2023-08-26 14:29:37 +0800

    new go

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 import ( 
 	"fmt"
-	//tool "myProject/tool"
+	tool "myProject/tool"
 )
 
 func main(){
-	/*
+	
 	var a int
 	fmt.Println(tool.Add(5,3))
 	fmt.Println(tool.GetStructure())
 	fmt.Println(tool.DeferTest())
 	fmt.Println("please input index you want to delete:")
 	fmt.Scanf("%d",&a)
-	fmt.Println(tool.SliceDelete(a))*/
+	fmt.Println(tool.SliceDelete(a))
 
 
 	var slice = make([] int, 5,10)//長度為5，空間為10
